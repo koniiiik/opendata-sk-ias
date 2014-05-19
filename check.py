48480668c6fee40efafce6b181033bfc868e0c14
Eduard Eiben <eduard.eiben@gmail.com>
2014-05-19 17:07:16 +0200

    vygenerovana tabulka + upravy v kode.

--- check.py
+++ check.py
@@ -11,19 +11,21 @@ import mimetypes
 
 four_star = ['rdf']
 three_star = ['xml', 'xsd', 'csv']
-two_star = ['xls']
+two_star = ['xls', 'xlsx']
+one_star = ['doc', 'pdf', 'zip', 'gz', 'txt', 'docx', 'rtf'] # to je nadmnozina toho, co realne je teraz v tych datasetoch
 
 def translate(ext, mime_type=None):
   if ext == 'wsdl':
     return 'xml'
   return ext
 
-all_formats = two_star + three_star + four_star
+all_formats = one_star + two_star + three_star + four_star
 
 def get_file_ext(mime_type, url, cont_disp):
   ext = None
   if cont_disp != None:
-    ext = cont_disp.split("filename=")[1].strip('\'"')
+    filename = cont_disp.split("filename=")[1].strip('\'"')
+    ext = filename.split('.')[-1]
     if ext in all_formats:
       return ext
   ext = url.split('.')[-1]
@@ -55,27 +57,36 @@ conn = sqlite3.connect('datasety.db')
 
 conn.isolation_level = None
 
+conn.execute("delete from ckan_resource;")
 req = requests.get('http://data.gov.sk/api/1/rest/dataset')
 assert req.status_code == requests.codes.ok
 
 response_dict = req.json()
 
+bad_datasets = ['register-obhospodarovatelov-lesa', # bad url - http:/g ...
+'digitalna-mapa-skol', # Incomplete read
+'naplnenost-skol-a-moznosti-studia' # Incomplete read
+]
+
 for ds_name in response_dict:
+  if ds_name in bad_datasets:
+    continue
   req1 = requests.get('http://data.gov.sk/api/1/rest/dataset/' + ds_name)
   dataset = req1.json()
   ds_id = dataset['id']
   resources = dataset['resources'] 
   for ds_metadata in resources:
     url =  ds_metadata['url'] # id, name, 
+    #~ print(ds_name, url)
     req2 = requests.get(url)
     headers = req2.headers
     content_type = headers['Content-Type']
     mime_type = content_type.split(';')[0]
     cont_disp = None
-    if 'content-disposition' in headers:
-      cont_disp = headers['content-disposition']
-    #~ else:
-      #~ print(req2.url.split('.')[-1])
+    lower_headers = {k.lower():v for k,v in headers.items()}
+    if 'content-disposition' in lower_headers:
+      cont_disp = lower_headers['content-disposition']
+    #~ print(req2.url)
     if (req2.status_code != requests.codes.ok):
       print (FAIL + "ERR (" + str(req2.status_code) + ")" + ENDC + " pre dataset:" + ds_name + ", url=" + url)
       conn.execute("""
@@ -91,13 +102,17 @@ for ds_name in response_dict:
       ))
     else:
       if 'text/html' in content_type:
-        print (FAIL + "NOT A DATASET" + ENDC + " pre dataset:" + ds_name + ", url=" + url)
+        if req2.is_redirect:
+          err_c = "REDIRECT"
+        else:
+          err_c = "WEB FORM?"
+        print (FAIL + err_c + ENDC + " pre dataset:" + ds_name + ", url=" + url)
         conn.execute("""
           INSERT into ckan_resource VALUES (?,?,?,?,?,?,?)
           """,(
            ds_id, 
            ds_name,
-           "NOT A DATASET",
+           err_c,
            url,
            0,
            None,
